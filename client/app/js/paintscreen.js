397571efe75d7a91fa6d319ab770ec657844477c
jarek <Kanapka@Edimmu>
2015-01-14 18:46:12 +0100

    canvas animation fix

--- client/app/js/paintscreen.js
+++ client/app/js/paintscreen.js
@@ -32,9 +32,31 @@ paintscreen.directive('semiCanvas', ['$parse', function($parse) {
   };
 }]);
 
+var canvasArr = [];
+function getFreeCanvas(){
+    var maxCanvasSize = 5;
+    if ( canvasArr.length < maxCanvasSize) {
+           var canvas = jQuery("<canvas width='600' height='400'>");
+           canvasArr.push(canvas);
+           canvas.addClass("animated");
+           return canvas;
+    } else {
+        var canvas  = canvasArr[0];
+        canvasArr.shift();
+        canvas.removeClass("animated");
+        canvas.addClass("animated");
+        canvasArr.push(canvas);
+        return canvas;
+    }
+     
+    
+}
+
 function drawWave(data){
-           var canvas = jQuery("<canvas width='1000' height='1000'>");
            
+           var canvas = getFreeCanvas();
+           var canvasWidth= 600;
+           var canvasHeight= 400;
            canvas.width("100%");
            canvas.height("100%");
          //  jQuery("article").append(canvas);
@@ -53,12 +75,12 @@ function drawWave(data){
             ctx.beginPath();
             ctx.strokeStyle = '#ff0000';
             ctx.lineWidth = 10;
-            ctx.moveTo(0,500);
+            ctx.moveTo(0,canvasHeight/2);
 
-            var dx  = 1000/data.length;
-            var dy = 300;
+            var dx  = canvasWidth/data.length;
+            var dy = 200;
             for ( var i = 0 ; i< data.length; i++ ) {
-                  ctx.lineTo( dx*i, 500+(data[i]*dy) );
+                  ctx.lineTo( dx*i, canvasHeight/2+(data[i]*dy) );
               
                
             }
