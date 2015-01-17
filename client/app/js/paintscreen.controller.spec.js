7182b9d25ed7df33307c44265ae78e0262892904
jarek <Kanapka@Edimmu>
2015-01-17 12:09:51 +0100

    controller sample

--- client/app/js/paintscreen.controller.spec.js
+++ client/app/js/paintscreen.controller.spec.js
@@ -1,2 +1,38 @@
+describe('paintscreen service', function () {
+    var paintService = null;
+    var controller=null;
+    var scope = null;
+    beforeEach(function () {
+        module("paintscreen");
+        paintService = {
+           getState : function() { } ,
+           registerObserver : function() { } 
+        };
+    });
+    
+        describe('paintCtrl', function () {
+        beforeEach(inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();
+            var timeout = null; 
+            
+            controller = $controller('paintCtrl', {
+                '$scope': scope,
+                '$timeout':timeout,
+                'paintService':paintService
+                
+            });
+        }));
+        
 
+        it('sets draw to true', function () {
+            expect(scope.draw).toBe(false);
+         
+            scope.$digest();
+            scope.mouseDown();
+              expect(scope.draw).toBe(true);
+        }); 
+    });
+
+}
+);
 
